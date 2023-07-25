d0bcc188f5bd985043b3873fe5899b5c986e28ac
Millicent Ama Adjubel <adjubelmillicent@gmail.com>
2023-07-25 23:39:58 +0000

    Academy project

--- form.js
+++ form.js
@@ -1,33 +1,36 @@
 class FormHandler {
-    async validateForm() {
-      const form = document.getElementById('myForm');
-      if (form.checkValidity()) {
-        // All fields are filled
-        return true;
-      } else {
-        // Some fields are missing, show an error message or handle it as needed
-        return false;
-      }
-    }
+  async validateForm() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const age = document.getElementById('age').value;
+    const country = document.getElementById('country').value;
 
-    async generateSentence() {
-      const firstName = document.getElementById('firstName').value;
-      const lastName = document.getElementById('lastName').value;
-      const age = document.getElementById('age').value;
-      const country = document.getElementById('country').value;
-
-      if (await this.validateForm()) {
-        const sentence = `My name is ${firstName} ${lastName}. I am from ${country}. I am ${age} years old.`;
-        console.log(sentence);
-      } else {
-        console.log('Please fill in all the fields.');
-      }
+    
+    if (!firstName || !lastName || !age || !country) {
+      throw new Error('All fields are required.');
     }
   }
 
-  const formHandler = new FormHandler();
-  const form = document.getElementById('myForm');
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    formHandler.generateSentence();
-  });
+  generateSentence() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const age = document.getElementById('age').value;
+    const country = document.getElementById('country').value;
+
+    const sentence = `My name is ${firstName} ${lastName}. I am from ${country}. I am ${age} years old.`;
+    return sentence;
+  }
+}
+
+const formHandler = new FormHandler();
+
+document.getElementById('myForm').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  try {
+    await formHandler.validateForm();
+    const sentence = formHandler.generateSentence();
+    alert(sentence);
+  } catch (error) {
+    alert(error.message);
+  }
+});
--- project.js
+++ project.js
@@ -17,3 +17,27 @@ function fizzBuzz(n) {
 console.log(fizzBuzz(30))
 
 
+
+
+
+function theAscending(arr) {
+    return arr.slice().sort((a, b) => a - b);
+  }
+  
+  function theDescending(arr) {
+    return arr.slice().sort((a, b) => b - a);
+  }
+  
+const unsortedArray = [1, 6, 7, 7, 0, 9, 7, 2, 5, 6, 4];
+const Ascending = theAscending(unsortedArray);
+const Descending = theDescending(unsortedArray);
+ 
+console.log(`Sorted in ascending order:, ${Ascending}`);
+console.log(`Sorted in descending order:, ${Descending}`);
+  
+  
+  
+  
+  
+  
+  
