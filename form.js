bef7e8b70a19aa8a19253c40d67c7f3f15fad109
Millicent Ama Adjubel <adjubelmillicent@gmail.com>
2023-07-26 00:22:07 +0000

    Academy project1

--- form.js
+++ form.js
@@ -1,36 +1,37 @@
-class FormHandler {
-  async validateForm() {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const age = document.getElementById('age').value;
-    const country = document.getElementById('country').value;
+document.addEventListener('DOMContentLoaded', () => {
+  class FormHandler {
+    async validateForm() {
+      const firstName = document.getElementById('firstName').value;
+      const lastName = document.getElementById('lastName').value;
+      const age = document.getElementById('age').value;
+      const country = document.getElementById('country').value;
 
-    
-    if (!firstName || !lastName || !age || !country) {
-      throw new Error("All fields are required.");
+      if (!firstName || !lastName || !age || !country) {
+        throw new Error("All fields are required.");
+      }
+    }
+
+    generateSentence() {
+      const firstName = document.getElementById('firstName').value;
+      const lastName = document.getElementById('lastName').value;
+      const age = document.getElementById('age').value;
+      const country = document.getElementById('country').value;
+
+      const sentence = `My name is ${firstName} ${lastName}. I am from ${country}. I am ${age} years old.`;
+      return sentence;
     }
   }
 
-  generateSentence() {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const age = document.getElementById('age').value;
-    const country = document.getElementById('country').value;
+  const formHandler = new FormHandler();
 
-    const sentence = `My name is ${firstName} ${lastName}. I am from ${country}. I am ${age} years old.`;
-    return sentence;
-  }
-}
-
-const formHandler = new FormHandler();
-
-document.getElementById('myForm').addEventListener('submit', async (event) => {
-  event.preventDefault();
-  try {
-    await formHandler.validateForm();
-    const sentence = formHandler.generateSentence();
-    alert(sentence);
-  } catch (error) {
-    alert(error.message);
-  }
+  document.getElementById('myForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    try {
+      await formHandler.validateForm();
+      const sentence = formHandler.generateSentence();
+      alert(sentence);
+    } catch (error) {
+      alert(error.message);
+    }
+  });
 });
